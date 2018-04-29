fa9ac7b4dbb84d35e50abe67ed6eebf7c930f39e
leo <zxc1213@163.com>
2018-04-29 20:06:55 +0800

    Initial commit

---
+++ src/zxc/tour/bean/App.java
@@ -0,0 +1,47 @@
+package zxc.tour.bean;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        Configuration cfg = new Configuration();
+        cfg.configure("source/Hibernate.cfg.xml");
+        sessionFactory = cfg.buildSessionFactory();
+    }
+
+    public void testSave() throws Exception {
+        User user = new User();
+        user.setName("zasan");
+
+        Session session = sessionFactory.openSession();  //打开一个新的session
+        Transaction tx = session.beginTransaction();  //开始事务
+
+        session.save(user);
+
+        tx.commit();  //提交事务
+        session.close();  //关闭session，释放资源
+    }
+
+    public void testGet() throws Exception {
+        Session session =sessionFactory.openSession();
+        User user = session.get(User.class,1);
+        System.out.println(user.getName());
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        try {
+            app.testSave();
+            app.testGet();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
